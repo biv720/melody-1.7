3e0567a20df22095a8470d3dcdf46b308219348f
biv720 <76109778+biv720@users.noreply.github.com>
2022-10-04 23:19:58 +0530

    Updated to main.py to py-cord

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+#Requires py-cord and uninstall dpy
+
 import os
 import discord
 from discord.ext import commands
 import jishaku
 from asyncio import sleep
 
+
+
 def main():
+    
+
     intents = discord.Intents.all()
     
 
@@ -12,10 +18,14 @@ def main():
     bot = commands.Bot(command_prefix= commands.when_mentioned_or('m-'), intents = discord.Intents.all())
     bot.remove_command('help')
     
+    
    
     token ="Insert Token"
 
     @bot.event
+    async def on_ready():
+        print("ready to go")
+
     async def Status():
         while True:
             await bot.wait_until_ready()
@@ -25,12 +35,19 @@ def main():
             await sleep(6)
     bot.loop.create_task(Status())
 
+    @bot.slash_command(name="test", description="This is just a test command to check if the bot is online or not")
+    async def test(ctx):
+        await ctx.respond(f'{bot.user} is online')
+
+    
+    
+
 
 
 
         
     
-    for filename in os.listdir('./mldy'):
+    for filename in os.listdir('./melody/mldy'):
         if filename.endswith('.py'):
             bot.load_extension(f'mldy.{filename[:-3]}')
     
